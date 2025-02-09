b2b0ef996f2e01917815f0299d373fe4d3990b51
anemonemon <monika.skink@gmail.com>
2025-02-09 15:29:00 +0200

    Pvz

--- main.py
+++ main.py
@@ -1,16 +1,97 @@
-# This is a sample Python script.
+import math
+import random
+import string
+from itertools import count
+from operator import index
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# FizzBuzz uzduotis
 
+fzsarasas = []
+for z in range(1,101):
+    fzsarasas.append(z)
+print(fzsarasas)
+for fz in fzsarasas:
+    if fz % 3 == 0 and fz % 5 == 0:
+        print('FizzBuzz')
+    elif fz % 3 == 0:
+        print('Fizz')
+    elif fz % 5 == 0:
+        print('Buzz')
+    else:
+        print(fz)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Veiksmai su dviem kintamaisiais
 
+while True:
+    print('Iveskite pirma skaiciu:')
+    sk = int(input())
+    print('Iveskite antra skaiciu:')
+    sk2 = int(input())
+    print('Irasykite norima veiksma - sudetis, atimtis, daugyba ar dalyba:')
+    veiksmas = input()
+    if veiksmas == 'sudetis':
+        print(sk, '+', sk2, '=', sk + sk2)
+    if veiksmas == 'atimtis':
+        print(sk, '-', sk2, '=', sk - sk2)
+    if veiksmas == 'daugyba':
+        print(sk, '*', sk2, '=', sk * sk2)
+    if veiksmas == 'dalyba':
+        print(sk, '/', sk2, '=', sk / sk2)
+    user_input = input('Ar norite pakartoti is naujo? (taip/ne): ')
+    if user_input.lower() in ["taip", "t"]:
+        continue
+    elif user_input.lower() in ["ne", "n"]:
+        break
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Random skaiciai ir ju vidurkiai
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+skaiciai = []
+for a in range(50):
+    skaiciai.append(random.randint(1, 10000))
+print(skaiciai)
+print('Vidurkis:', sum(skaiciai)/len(skaiciai))
+print('Didziausias skaicius:', max(skaiciai))
+print('Maziausias skaicius:', min(skaiciai))
+mazesni = []
+for sk in skaiciai:
+    if sk < sum(skaiciai)/len(skaiciai):
+        mazesni.append(sk)
+mazesni.sort(reverse=True)
+print('Mazesni skaiciai uz vidurki:', mazesni)
+print('Bei ju vidurkis:', round(sum(mazesni)/len(mazesni), 2))
+didesni = []
+for sk2 in skaiciai:
+    if sk2 > sum(skaiciai)/len(skaiciai):
+        didesni.append(sk2)
+didesni.sort()
+print('Didesni skaiciai uz vidurki:', didesni)
+print('Bei ju vidurkis:', round(sum(didesni)/len(didesni), 2))
+
+# fukcijos - max reiksmes paieska bei sarasu uzpildymas random skaiciais
+
+def didz(listas):
+    maxi = listas[0]
+    for sk in listas:
+        if sk > maxi:
+            maxi = sk
+    print('didziausias rastas skaicius:', maxi)
+
+def skaic(listukas):
+        listukas.append(random.randint(1,100))
+        listukas.append(random.randint(1, 100))
+        listukas.append(random.randint(1, 100))
+        listukas.append(random.randint(1, 100))
+        listukas.append(random.randint(1, 100))
+        print(listukas)
+
+pirmas = []
+antras = []
+trecias = []
+
+skaic(pirmas)
+skaic(antras)
+skaic(trecias)
+
+didz(pirmas)
+didz(antras)
+didz(trecias)
